193789e8a2e338b501c8406132b754c8455afbec
DinkleArora <dinkle2803@gmail.com>
2026-06-30 21:41:51 +0530

    30Jun

---
+++ src/models/user-models.js
@@ -0,0 +1,75 @@
+import mongoose, { Scheme } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: "",
+        localPath: "",
+      },
+    },
+
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    fullName: {
+      type: String,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    forgotPasswordToken: {
+      type: String,
+    },
+
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+
+    emailVerificationToken: {
+      type: String,
+    },
+
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+export const User = monngoose.model("User", userSchema)
