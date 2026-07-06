21fc9396a589bcab708b5b094543bb904d403cc6
DinkleArora <dinkle2803@gmail.com>
2026-07-06 17:03:35 +0530

    6 Jul

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth_controllers.js";
+import { login, registerUser } from "../controllers/auth_controllers.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { userRegisterValidator } from "../validators/index.js";
+
 const router = Router();
 
-router.route("/register").post(registerUser)
+
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(userLoginValidator(), validate, registerUser);
+
+router.route("/login").post(login);
 
 export default router;
