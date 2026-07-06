7afecede7bbb6c586ec9779a2bcac406b05b86df
DinkleArora <dinkle2803@gmail.com>
2026-07-06 23:11:17 +0530

    6 Jul

--- src/validators/index.js
+++ src/validators/index.js
@@ -23,8 +23,22 @@ const userRegisterValidator = () => {
         body("fullName")
             .optional()
             .trim(),
+
     ];
 }
+
+const userLoginValidator = () => {
+    return [
+        body("email")
+        .optional()
+        .isEmail()
+        .withMessage("Email is invalid"),
+        body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+    ];
+};
+
 
 export {
     userRegisterValidator
